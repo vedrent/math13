ed69d9660f7871df7bb04965d125e597378be577
vedro <vedrentoff@gmail.com>
2024-12-05 21:50:25 +0300

    added audio

--- MainForm.cs
+++ MainForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NAudio.Wave;
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Imaging;
@@ -8,6 +9,70 @@ using System.Windows.Forms;
 
 namespace math13
 {
+    public class SoundPlayer
+    {
+        private WaveOutEvent outputDevice;
+        private AudioFileReader audioFile;
+
+        public void Play(string filePath, bool loop = false)
+        {
+            //Stop(); // Останавливаем текущий звук, если проигрывается
+
+            var reader = new AudioFileReader(filePath);
+            audioFile = reader;
+            outputDevice = new WaveOutEvent();
+
+
+            outputDevice.Init(audioFile);
+            outputDevice.Play();
+        }
+
+        public void Stop()
+        {
+            outputDevice?.Stop();
+            outputDevice?.Dispose();
+            audioFile?.Dispose();
+
+            outputDevice = null;
+            audioFile = null;
+        }
+
+        // Вложенный класс для зацикливания звука
+        private class LoopStream : WaveStream
+        {
+            private readonly WaveStream sourceStream;
+
+            public LoopStream(WaveStream sourceStream)
+            {
+                this.sourceStream = sourceStream;
+                this.EnableLooping = true;
+            }
+
+            public bool EnableLooping { get; set; }
+
+            public override WaveFormat WaveFormat => sourceStream.WaveFormat;
+
+            public override long Length => sourceStream.Length;
+
+            public override long Position
+            {
+                get => sourceStream.Position;
+                set => sourceStream.Position = value;
+            }
+
+            public override int Read(byte[] buffer, int offset, int count)
+            {
+                int read = sourceStream.Read(buffer, offset, count);
+                if (read == 0 && EnableLooping)
+                {
+                    sourceStream.Position = 0;
+                    read = sourceStream.Read(buffer, offset, count);
+                }
+                return read;
+            }
+        }
+    }
+
     public partial class MainForm : Form
     {
         private System.Windows.Forms.Timer gameTimer = new System.Windows.Forms.Timer();
@@ -18,6 +83,7 @@ namespace math13
         private List<Item> items = new List<Item>();
         private Random random = new Random();
         private HashSet<Keys> pressedKeys = new HashSet<Keys>();
+        private SoundPlayer soundPlayer = new SoundPlayer();
 
         private int score = 0;
         private bool gameOver = false;
@@ -43,6 +109,11 @@ namespace math13
 
         private Image rocketBoostImage;
         private Image explosionBoostImage;
+
+        private string explosionSoundPath;
+        private string spaceshipExplosionSoundPath;
+        private string shootSoundPath;
+        private string itemSoundPath;
 
         private void LoadExplosionFrames(Image gif)
         {
@@ -78,6 +149,10 @@ namespace math13
             rocketBoostImage = Image.FromFile("C:\\_coding\\progmath\\math13\\Images\\rocket_boost.png");
             explosionBoostImage = Image.FromFile("C:\\_coding\\progmath\\math13\\Images\\explosion_boost.png");
 
+            explosionSoundPath = "C:\\_coding\\progmath\\math13\\Sounds\\explosion.mp3";
+            spaceshipExplosionSoundPath = "C:\\_coding\\progmath\\math13\\Sounds\\spaceship_explosion.mp3";
+            shootSoundPath = "C:\\_coding\\progmath\\math13\\Sounds\\fire.mp3";
+            itemSoundPath = "C:\\_coding\\progmath\\math13\\Sounds\\item.mp3";
 
             //InitializeComponent();
             InitGame();
@@ -172,6 +247,7 @@ namespace math13
                         {
                             rockets.Remove(rocket); // Удаляем ракету после взрыва
                             explosions.Add(new Explosion(rocket.ExplosionArea, new AnimatedGif(explosionFrames, explosionDelays)));
+                            soundPlayer.Play(explosionSoundPath);
                             foreach (var hitComet in hitComets)
                             {
                                 hitComet.OnDestroyed(items);
@@ -192,6 +268,7 @@ namespace math13
                     deathTimer = 65;
                     spaceship.Direction = new Point(spaceship.Direction.X / 3, spaceship.Direction.Y / 3);
                     explosions.Add(new Explosion(new Rectangle(spaceship.X, spaceship.Y, 50, 50), new AnimatedGif(explosionFrames, explosionDelays)));
+                    soundPlayer.Play(spaceshipExplosionSoundPath);
                 }
             }
 
@@ -238,6 +315,7 @@ namespace math13
 
         private void HandleItemCollection(Item item)
         {
+            soundPlayer.Play(itemSoundPath);
             if (item.Type == 0)
             {
                 rocketCount ++;
@@ -271,6 +349,7 @@ namespace math13
             if (e.KeyCode == Keys.Space && rockets.Count < rocketCount)
             {
                 rockets.Add(new Rocket(spaceship.X + 20, spaceship.Y - 10, explosionRadius));
+                soundPlayer.Play(shootSoundPath);
             }
         }
 
